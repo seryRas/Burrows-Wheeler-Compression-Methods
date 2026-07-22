be5a2e99cb5cb5e94310e640b1b7d32b41522f93
Rastislav Šerý <xseryra00@stud.fit.vut.cz>
2026-07-22 20:00:11 +0200

    began working on tests for bigger files

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include "BWT.h"
 
-int main() {
-    char arr[] = "banana";
 
-    bwt_out out = {.size = sizeof(arr) - 1};
-    if ((out.data = malloc(sizeof(arr) - 1)) == NULL) return mallocErr;
+int main(int argc, char **argv) {
+    FILE *testInput = fopen(argv[1], "r");
+    char c;
+    size_t sizeArr = 0x4FFFFFF;
+    unsigned char *arr = malloc(sizeArr);
+    size_t i = 0;
+
+    while((c = getc(testInput)) != EOF) {
+        arr[i++] = c;
+        if(i >= sizeArr) {
+            fprintf(stderr, "File too big\n");
+            return 1;
+        }
+    }
+    
+
+
+    bwt_out out = {.size = i - 1};
+    if ((out.data = malloc(i - 1)) == NULL) return mallocErr;
     bwtTransform((unsigned char*)arr, &out);
-    for (unsigned int i = 0; i < sizeof(arr) - 1; i++)
+    for (unsigned int i = 0; i < i - 1; i++)
         printf("%c", out.data[i]);
     printf("\n");
-    unsigned char* output = malloc(sizeof(arr));
+    unsigned char* output = malloc(i);
     bwtRetransform(&out, output);
-    printf("%s\n", output);
+    fprintf(stdout, "%s\n", output);
     return 0;
 }
